8dd72e091ae53dafdf49b1522ff285a4b2757dee
Arjun Das <arjun_itech@yahoo.co.in>
2026-01-09 03:15:02 +0530

    feature of feature- but final commit   commit not  yet pushed

--- Simple_Class.java
+++ Simple_Class.java
@@ -19,7 +19,7 @@ public class Simple_Class {
 		
 		System.out.println("Hello World"+Stringo.toString());
 		System.out.println("first   cut "+Stringo.toString());
-		System.out.println("first   cut- feature of feature - but secodcommit -but third   commmit  "+Stringo.toString());
+		System.out.println("first   cut- feature of feature - but secodcommit -but third   commmit  -  final   commit  "+Stringo.toString());
 
 
 
